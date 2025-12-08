686daca8fed2c3ba356966ece180db8a0357c5cc
Ashwath shiva p <ashwath0598@gmail.com>
2025-12-08 19:52:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -136,4 +136,6 @@ def index():
     return render_template_string(HTML)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port, threaded=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+
